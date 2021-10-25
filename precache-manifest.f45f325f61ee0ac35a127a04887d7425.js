8e13095d3a0805d513612986a18f80c6889a8ecb
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2021-10-26 03:28:18 +0800

    Updates

---
+++ precache-manifest.f45f325f61ee0ac35a127a04887d7425.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1df0a61563fc35f12437",
+    "url": "/acgen/static/css/main.3e42e066.chunk.css"
+  },
+  {
+    "revision": "1df0a61563fc35f12437",
+    "url": "/acgen/static/js/main.9db716cc.chunk.js"
+  },
+  {
+    "revision": "0a228a5857b65c1e6ac9",
+    "url": "/acgen/static/js/polyfills-dom.a65be99d.chunk.js"
+  },
+  {
+    "revision": "ae4379250de47c18d91f",
+    "url": "/acgen/static/js/runtime~main.61493a01.js"
+  },
+  {
+    "revision": "5f4236c9e53babdad318",
+    "url": "/acgen/static/js/3.cb4b4912.chunk.js"
+  },
+  {
+    "revision": "a0d7e0e2c8e928d940b6",
+    "url": "/acgen/static/js/4.cf815a83.chunk.js"
+  },
+  {
+    "revision": "b7b6d2daf643216d2825",
+    "url": "/acgen/static/js/5.2703f3e9.chunk.js"
+  },
+  {
+    "revision": "d8ad4cf5362274f680138b7b99d5302e",
+    "url": "/acgen/static/media/avatar-sena.d8ad4cf5.png"
+  },
+  {
+    "revision": "0e11b51d7eed266e91c10b59fd339e20",
+    "url": "/acgen/static/media/avatar-diep.0e11b51d.png"
+  },
+  {
+    "revision": "ab9600b43f76b52b87b758c5528eff7f",
+    "url": "/acgen/index.html"
+  }
+];
